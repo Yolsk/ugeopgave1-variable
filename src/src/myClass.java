9d4c2f0ba095d2ff890ebaadf8556285b6fda92a
jakob <cph-jc341@stud.ek.dk>
2026-09-02 20:12:55 +0200

    Resten af opgaverne afleveret.

--- src/src/myClass.java
+++ src/src/myClass.java
@@ -3,7 +3,7 @@ public class myClass {
 
     void main() {
 
-        // opgave 1
+        /* opgave 1
 
         double itemOne = 25.50;
         double itemTwo = 15.00;
@@ -21,11 +21,137 @@ public class myClass {
         double tip = 50;
         int persons = 4;
         double totalBill = food + drinks + tip;
-        double pricePerPerson = totalBill / 4;
+        double pricePerPerson = totalBill / persons;
 
         System.out.println(pricePerPerson);
 
+
+
         // opgave 3
+        double price = 300.0;
+        double discountPercent = 20.0;
+        double discountPrice = price - (price * discountPercent / 100);
+        double taxes = 25.0;
+        double totalSum = discountPrice + (discountPrice * taxes / 100);
+        System.out.println("Original Price: " + price);
+        System.out.println("Discounted Price: " + discountPrice);
+        System.out.println("Final Price: " + totalSum);
+
+        // opgave 4
+        int guests = 15;
+        int cupcakes = 40;
+        int cupcakePerPerson = cupcakes / guests;
+        int leftoverCupcakes = cupcakes % guests;
+        System.out.println(cupcakePerPerson);
+        System.out.println(leftoverCupcakes);
+
+        // opgave 5
+        StringBuilder sb: = new StringbBuilder()
+        String name = "Anna";
+        int age = 25;
+        sb.append("Hello, ")
+        sb.append(name)
+        sb.append("!")
+        sb.append("You are")
+        sb.append(age)
+        sb.appned("years old")
+        sb.toString()
+
+        // opgave 6
+        int score = 100;
+        System.out.println(score);
+        score += 50;
+        System.out.println(score);
+        score += 25;
+        System.out.println(score);
+
+        // opgave 7
+        int health = 100;
+        health -= 15;
+        System.out.println(health);
+        health -= 20;
+        System.out.println(health);
+
+
+        // opgave 8
+        double price = 50.0;
+        System.out.println(price);
+        price *= 2;
+        System.out.println(price);
+
+        // opgave 9
+        int count = 10;
+        System.out.println(count);
+        count++;
+        System.out.println(count);
+        count--;
+        System.out.println(count);
+
+        // opgave 10
+        int number = 10;
+        number+= 5;
+        System.out.println(number);
+        number-= 3;
+        System.out.println(number);
+        number++;
+        System.out.println(number);
+        number--;
+        System.out.println(number);
+
+        // opgave 11
+        String item = "Laptop";
+        double price = 7999.99;
+        boolean inStock = true;
+        int itemsLeft = 15;
+        double itemDiscount = 10.0;
+        String category = "Electronics";
+
+        // opgave 12
+        String name = "Anna";
+        int testPoints = 85;
+        int assignmentPoints = 92;
+        double averageGrade = 88.5;
+        boolean hasPassed = true;
+
+        // opgave 13
+        String guestName = "Peter Hansen";
+        int roomNumber = 204;
+        int nightStay = 3;
+        double pricePerNight = 899.00;
+        boolean hasBreakfast = true;
+        double totalPrice = 2697.00;
+
+        // opgave 14
+        int stepsToday = 8547;
+        int StepsGoal = 10000;
+        double distance = 6.2;
+        int caloriesBurned = 420;
+        boolean goalReached = false;
+
+        // opgave 15
+        String bookTitle = "Mistborn";
+        String author = "Brandon Sanderson";
+        int ISBN = 100;
+        int pagesTotal = 700;
+        boolean canBeBorrowed = true;
+        String currentBorrower = "Simsalabim";
+        double fee = 30;
+        boolean isBestseller = true;
+        double rating = 5.0;
+
+         */
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
