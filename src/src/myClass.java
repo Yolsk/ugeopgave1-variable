869a64386fd7a4c01920d1ec218948fd8e3ecfbf
jakob <cph-jc341@stud.ek.dk>
2026-09-02 15:59:10 +0200

    Opgave 1 og 2 tilføjet

--- src/src/myClass.java
+++ src/src/myClass.java
@@ -3,7 +3,31 @@ public class myClass {
 
     void main() {
 
-        System.out.println("Hej");
+        // opgave 1
+
+        double itemOne = 25.50;
+        double itemTwo = 15.00;
+        double itemThree = 30.00;
+        double subTotal = itemOne + itemTwo + itemThree;
+        double taxes = 25.0;
+        double withTaxes = subTotal + (subTotal * taxes / 100.0);
+
+        System.out.println(subTotal);
+        System.out.println(withTaxes);
+
+        // opgave 2
+        double food = 450.0;
+        double drinks = 120.0;
+        double tip = 50;
+        int persons = 4;
+        double totalBill = food + drinks + tip;
+        double pricePerPerson = totalBill / 4;
+
+        System.out.println(pricePerPerson);
+
+        // opgave 3
+
+
     }
 
 
